1baa10a0ca6aa700a72f0927464569b3c2cacb48
jindalhackerrank <jindalhackerrank@gmail.com>
2014-09-28 14:59:28 +0530

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,32 @@
+## It is used for reading test data set
+q<-read.table("UCI/test/X_test.txt")
+
+##It is used for reading the X traing data set
+w<-read.table("UCI/train/X_train.txt")
+
+## It is used for merging the data
+e<-rbind(q,w)
+
+##It is used for getting the dimensions of data
+dim(e)
+
+##It is used for reading features data
+a<-read.table("UCI/features.txt")
+
+##it is used for taking only names 
+s<-a[,2]
+
+## It is used for taking only the variables with mean() and std()
+d<-grep("mean()|std()",s)
+
+##It is used to extract columns with means and std() from the data set
+r<-e[d]
+
+##It is used for assigning names to the columns
+names(r)<-s[d]
+
+## It is used to calculate means of variable accross columns
+b<-colMeans(r)
+
+##It is used for writing the result into file .
+write.table(b,file="result.txt",row.names=FALSE)
